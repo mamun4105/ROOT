0c85981f6c59c0bf9e9e33517dfb1f4273cf936e
Saleh Mamun <mamun4105@gmail.com>
2021-12-08 01:22:08 -0600

    update dec

--- natcap/root/__init__.py
+++ natcap/root/__init__.py
@@ -6,7 +6,8 @@ LOGGER = logging.getLogger('natcap.root')
 LOGGER.addHandler(logging.NullHandler())
 
 try:
-    __version__ = pkg_resources.get_distribution(__name__).version
+    # __version__ = pkg_resources.get_distribution(__name__).version
+    __version__ = 's.0.1'
 except pkg_resources.DistributionNotFound:
     # package is not installed.  Log the exception for debugging.
     LOGGER.exception('Could not load natcap.root version information')
